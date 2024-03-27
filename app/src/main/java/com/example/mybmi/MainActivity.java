ff879d09ede505483c391bcf2900caf56be031db
JungChi Lin <111B13929@mailst.cjcu.edu.tw>
2024-03-28 02:05:13 +0800

    計算BMI的功能，calcBMI

--- app/src/main/java/com/example/mybmi/MainActivity.java
+++ app/src/main/java/com/example/mybmi/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mybmi;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    public void calcBMI(View view){
+        EditText height = findViewById(R.id.etHeight);
+        EditText weight = findViewById(R.id.etWeight);
+        double h = Double.parseDouble(height.getText().toString());
+        double w = Double.parseDouble(weight.getText().toString());
+        double bmi = w / (h/100.0 * h/100.0);
+        String result = "你的BMI是" + bmi;
+        TextView show = findViewById(R.id.tvShow);
+        show.setText(result);
     }
 }
