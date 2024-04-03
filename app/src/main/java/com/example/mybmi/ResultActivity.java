d23228b1d817976f1b7b1481bf7e8e00d162c5bc
JungChi Lin <111B13929@mailst.cjcu.edu.tw>
2024-04-03 17:36:42 +0800

    新增返回按鈕功能

--- app/src/main/java/com/example/mybmi/ResultActivity.java
+++ app/src/main/java/com/example/mybmi/ResultActivity.java
@@ -1,6 +1,7 @@
 package com.example.mybmi;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,14 +21,18 @@ public class ResultActivity extends AppCompatActivity {
         ImageView img = findViewById(R.id.ivShow);
         double bmi = getIntent().getDoubleExtra("bmi", 0);
         if (bmi<18.5){
-            //show.setText("體重過輕");
-            img.setImageResource(R.drawable.a1);
+            show.setText("體重過輕");
+            img.setImageResource(R.drawable.a3);
         }else if (bmi<=24){
-            //show.setText("體重正常");
+            show.setText("體重正常");
             img.setImageResource(R.drawable.a2);
         }else if (bmi>24){
-            //show.setText("體重過重");
-            img.setImageResource(R.drawable.a3);
+            show.setText("體重過重");
+            img.setImageResource(R.drawable.a1);
         }
+    }
+
+    public void Goback(View view) {
+        finish();
     }
 }
