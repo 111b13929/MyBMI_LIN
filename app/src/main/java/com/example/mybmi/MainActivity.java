4125da224cbe02aa165a4bb8b57e84065439dd9a
JungChi Lin <111B13929@mailst.cjcu.edu.tw>
2024-04-03 18:55:01 +0800

    CheckBox 的用法

--- app/src/main/java/com/example/mybmi/MainActivity.java
+++ app/src/main/java/com/example/mybmi/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mybmi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -20,24 +22,58 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup rgSex;
     private RadioButton rbMale;
     private RadioButton rbFemale;
+    private CheckBox apple;
+    private CheckBox banana;
+    private CheckBox orange;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
 
-        rgSex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        rgSex.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.rbMale){
+                show.setText("我是男生");
+            }
+            else if (checkedId == R.id.rbFemale){
+                show.setText("我是女生");
+            }
+        });
+        apple.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.rbMale){
-                    show.setText("我是男生");
-                }
-                else if (checkedId == R.id.rbFemale){
-                    show.setText("我是女生");
-                }
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                getFruits();
+
+            }
+        });
+        banana.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                getFruits();
+            }
+        });
+        orange.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                getFruits();
             }
         });
 
+
+    }
+
+    private void getFruits() {
+        String msg ="";
+        if (apple.isChecked()){
+            msg += "蘋果";
+        }
+        if (banana.isChecked()){
+            msg += "香蕉";
+        }
+        if (orange.isChecked()){
+            msg += "橘子";
+        }
+        show.setText("我喜歡吃" + msg);
     }
 
     public void calcBMI(View view){
@@ -65,6 +101,9 @@ public class MainActivity extends AppCompatActivity {
         rgSex = findViewById(R.id.rgSex);
         rbMale = findViewById(R.id.rbMale);
         rbFemale = findViewById(R.id.rbFemale);
+        apple = findViewById(R.id.cbApple);
+        banana = findViewById(R.id.cbBanana);
+        orange = findViewById(R.id.cbOrange);
     }
 
     public void GoNext(View view) {
